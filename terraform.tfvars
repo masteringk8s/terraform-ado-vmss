ffe0175569f180adb3055b7fe4e41fd1616994ee
Everette McConnell <bmcconnell@Everettes-MacBook-Pro.local>
2021-03-26 17:12:32 -0400

    added tfvars

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,4 @@
 prefix = "5058"
 project-rg = "PROJECT-5058-RG"
+location = "eastus"
+vmsize = "Standard_B1ms"
--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "vmsize" {
   type                      = string
-  default                   = "Standard_B1ms"
   description               = "Size of VM to create"
 }
 
 variable "location" {
   type                      = string
-  default                   = "eastus"
   description               = "The Azure Region used"
 }
 
 variable "prefix" {
   type                      = string
   description               = "prefix to use for infra"
+}
+
+variable "project-rg" {
+  type                      = string
+  description               = "Resource group for project"
 }
