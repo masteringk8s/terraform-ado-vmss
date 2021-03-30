300f7efebe6ab4bdbeadfa76640e51693f3e87e5
Brent McConnell <brmcconn@microsoft.com>
2021-03-30 13:42:30 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,20 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-  backend "azurerm" {
-    storage_account_name  = "terraformstateebmsa"
-    container_name        = "terraformstate"
-    key                   = "terraform.tfstate.ado"
-  }
-}
+        required_providers {
+                azurerm = {
+                        source = "hashicorp/azurerm"
+                        version = "~>2.53.0"
+                }
+        }
+        backend "azurerm" {
+                storage_account_name = "terraformstate2429sa"
+                container_name = "terraformstate"
+                key = "terraform.state.main"
+        }
+ }
 
-provider "azurerm" {
-  skip_provider_registration = true
-  features {} 
-}
+ provider "azurerm" {
+        skip_provider_registration = true
+        features {}
+ }
 
 
