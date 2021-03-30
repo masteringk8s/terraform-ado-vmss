257c860c892b6f6b2e7a764610a82428286cb5ce
Brent McConnell <brmcconn@microsoft.com>
2021-03-30 13:51:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,10 @@ resource "azurerm_subnet" "internal" {
 resource "random_password" "password" {
   length           = 16
   special          = true
-  override_special = "_%@"
+  override_special = "_!-:=+*#"
+  upper            = true
+  number           = true
+  lower            = true
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "ado-vmss" {
